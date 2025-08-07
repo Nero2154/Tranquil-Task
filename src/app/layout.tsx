800c62e66b4835145a24c161d8fae03d51dbccaf
Sujal Soni <sujalsoni1221@gmail.com>
2025-08-07 10:05:17 +0000

    Make sure this is ready for pwabuilder, use this image I uploaded as the

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,7 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <link rel="icon" href="/tranquil_icon.png" sizes="any" />
         <link rel="apple-touch-icon" href="/tranquil_icon.png" />
+        <meta name="theme-color" content="#f8fafc" />
       </head>
       <body className="font-body antialiased">
         {children}
